112b741c5405ceeadba7766bf10d2b4b071fb300
Ali Murat El <ali_murat_el@hotmail.com>
2022-09-13 13:58:34 +0300

    update10

--- chatService.Server/Program.cs
+++ chatService.Server/Program.cs
@@ -2,7 +2,7 @@
 
 namespace ChatService.Server
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- chatService.Server/Server.cs
+++ chatService.Server/Server.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace ChatService.Server
+namespace ChatService.Server 
 {
     public class Server
     {
